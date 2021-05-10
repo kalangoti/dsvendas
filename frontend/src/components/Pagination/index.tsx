8abb058fd26681d408221f61bbf0a0992e49825a
Anderson Santo <kalangoti@gmail.com>
2021-05-09 21:01:45 -0300

    Pagination

---
+++ frontend/src/components/Pagination/index.tsx
@@ -0,0 +1,40 @@
+import { SalePage } from "types/sale";
+
+type Props = {
+  page: SalePage;
+  onChangePage: Function;
+};
+
+// Props
+// Argumentos que um componente React pode receber
+const Pagination = ({ page, onChangePage }: Props) => {
+  return (
+    <div className="row d-flex justify-content-center">
+      <nav>
+        <ul className="pagination">
+          <li className={`page-item ${page.first ? "disabled" : ""}`}>
+            <button
+              className="page-link"
+              onClick={() => onChangePage(page.number - 1)}
+            >
+              Anterior
+            </button>
+          </li>
+          <li className="page-item disabled">
+            <span className="page-link">{page.number + 1}</span>
+          </li>
+          <li className={`page-item ${page.last ? "disabled" : ""}`}>
+            <button
+              className="page-link"
+              onClick={() => onChangePage(page.number + 1)}
+            >
+              Próxima
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Pagination;
